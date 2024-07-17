f1919a9ee3822ea4e73bd6c3ca804c5f28de755b
Priyansh <priyanshpatel234@gmail.com>
2024-07-17 22:10:47 +0530

    changes

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -18,22 +18,24 @@ export const updateUser = async (req, res, next) => {
     req.body.password = bcryptjs.hashSync(req.body.password, 10);
   }
 
-  if (req.body.username.includes(" ")) {
-    return next(errorHandler(400, "User can not contain spaces"));
-  }
+  if (req.body.username) {
+    if (req.body.username.includes(" ")) {
+      return next(errorHandler(400, "User cannot contain spaces"));
+    }
 
-  if (req.body.username !== req.body.username.toLowerCase()) {
-    return next(errorHandler(400, "Username must be lowercase"));
-  }
+    if (req.body.username !== req.body.username.toLowerCase()) {
+      return next(errorHandler(400, "Username must be lowercase"));
+    }
 
-  if (req.body.username.match(/^[a-zA-z0-9] + $/)) {
-    return next(errorHandler(400, "Username only contain number and letter"));
-  }
+    if (req.body.username.match(/^[a-zA-z0-9]+ $ /)) {
+      return next(errorHandler(400, "Username only contain number and letter"));
+    }
 
-  if (req.body.username.length < 7 || req.body.username.length > 20) {
-    return next(
-      errorHandler(400, "username must be between 7 and 20 character")
-    );
+    if (req.body.username.length < 7 || req.body.username.length > 20) {
+      return next(
+        errorHandler(400, "username must be between 7 and 20 character")
+      );
+    }
   }
 
   try {
